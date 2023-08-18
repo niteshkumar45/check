087d07044793b3027f55e68f0285253a1e02f038
nitesh <niteshkymar1@gmail.com>
2023-08-18 09:24:32 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+let count = 0;
 
 app.get("/",(req,res)=>{
-    console.log("request on home page");
+    console.log(`request on home page ${count} time`);
+    count++;
     res.send("this is home page")
 })
 
